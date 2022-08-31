922bd045b820cfba575bfbd303fb6ca67f80ee95
Opilane TTHK <opilane@tthk.ee>
2022-08-31 10:46:00 +0300

    Code cleanup

--- javascript/app.js
+++ javascript/app.js
@@ -6,18 +6,27 @@ const submit = document.querySelector('#submit');
 submit.addEventListener('click',saveTask);
 
 function saveTask() {
-var node = document.createElement('li');
-node.setAttribute("onclick","struck(this)")
-node.setAttribute("class","fs-5")
+    if (document.getElementById('taskName').value === "" || document.getElementById('taskDate').value === "") {
+        alert("Please fill out the required inputs!");
+    }
+    else {
+        var node = document.createElement('li');
+        node.setAttribute("onclick","struck(this)")
+        node.setAttribute("class","fs-5")
+    
+    
+        var name = document.getElementById('taskName').value;
+        var date = document.getElementById('taskDate').value;
+    
+    
+        node.appendChild(document.createTextNode(name +" " + date));
+     
+        document.querySelector('ul').appendChild(node);
+    }
+}
 
-
-var name = document.getElementById('taskName').value;
-var date = document.getElementById('taskDate').value;
-
-
-node.appendChild(document.createTextNode(name +" " + date));
- 
-document.querySelector('ul').appendChild(node);
+function struck(ele) {
+    ele.style = "text-decoration:line-through;";
 }
 
 
